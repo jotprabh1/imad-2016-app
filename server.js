5319bab3c68b19c805aabf652c27dd7f64bcc0ce
jotprabh1 <prabhjot.narula@gmail.com>
2016-09-18 22:42:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,55 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+
+var articleOne = {
+    title: 'Article One | Prabhjot Singh',
+    heading: 'Article One',
+    date: 'Sep 5, 2016',
+    content: `<p>
+                    This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article.
+                </p>
+                <p>
+                    This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article.
+                </p>
+                <p>
+                    This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article. This is the contect of my first article.
+                </p>`
+};
+
+function CreateTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading = data.heading;
+    var content= data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>Home</a>
+                </div>
+                <h1 style="text-align:center;">${heading}</h1>
+                <hr/>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>    
+            </body>
+        </html>`;
+    return htmlTemplate;
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -10,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.sendFile(path.join(__dirname, 'article-one.html'));
+    res.send(CreateTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res){
